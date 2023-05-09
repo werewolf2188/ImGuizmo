66f324fe755ba731e91caf32623601039aeaac20
Enrique Ricalde <enrique.ricalde@walmart.com>
2023-05-09 15:24:30 -0500

    Adding premake

---
+++ premake5.lua
@@ -0,0 +1,51 @@
+project "ImGuizmo"
+	kind "StaticLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"GraphEditor.h",
+		"GraphEditor.cpp",
+		"ImCurveEdit.h",
+		"ImCurveEdit.cpp",
+		"ImGradient.h",
+		"ImGradient.cpp",
+		"ImGuizmo.h",
+		"ImGuizmo.cpp",
+		"ImSequencer.h",
+		"ImSequencer.cpp",
+		"ImZoomSlider.h"
+	}
+
+	includedirs
+   	{
+	    	"%{IncludeDir.ImGui}"
+   	}
+
+	filter "system:windows"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+	
+	filter "system:macosx"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+		flags { "C++14" }
+
+	filter "system:linux"
+		pic "On"
+		systemversion "latest"
+		cppdialect "C++17"
+		staticruntime "On"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
